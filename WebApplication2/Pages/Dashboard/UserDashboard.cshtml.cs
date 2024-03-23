9f7ecffcae1cd98cd87a98735063108d93081ae4
Nyrolla <alloryn.mappin@me.com>
2024-03-23 12:35:35 -0600

    Updated the profile and added comments

--- WebApplication2/Pages/Dashboard/UserDashboard.cshtml.cs
+++ WebApplication2/Pages/Dashboard/UserDashboard.cshtml.cs
@@ -1,5 +1,6 @@
 using DataAccess;
 using Infrastructure.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -13,20 +14,34 @@ namespace FireBnBWeb.Pages.Dashboard
     {
         //bring in the database
         private readonly UnitofWork _unitofWork;
+        private readonly UserManager<ApplicationUser> _userManager;
         public IEnumerable<Booking> bookingList;
         public IEnumerable<Property> propertyList;
         public IEnumerable<Location> locationList;
 
-        public UserDashboardModel(UnitofWork unitofWork)
+        public UserDashboardModel(UnitofWork unitofWork, UserManager<ApplicationUser> userManager)
         {
             _unitofWork = unitofWork;
+            _userManager = userManager;
             bookingList = new List<Booking>();
             propertyList = new List<Property>();
             locationList = new List<Location>();
 
         }
-        public IActionResult OnGet()
+        public string FullName { get; private set; }
+        public async Task<IActionResult> OnGetAsync()
         {
+            // Get the currently logged-in user
+            var user = await _userManager.GetUserAsync(User);
+
+            if (user == null)
+            {
+                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+            }
+
+            // Set the full name
+            FullName = $"{user.FirstName} {user.LastName}";
+
             return Page();
         }
     }
