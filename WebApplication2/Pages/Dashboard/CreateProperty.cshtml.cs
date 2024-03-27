91fbce72d604f5142e217ff589fb7d429b2c4bb3
rockstar12410 <austinallred5@mail.weber.edu>
2024-03-27 15:42:23 -0600

    Created page

---
+++ WebApplication2/Pages/Dashboard/CreateProperty.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FireBnBWeb.Pages.Dashboard
+{
+    public class CreatePropertyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
