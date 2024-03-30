771ea627809c4903cb654c97ef2e5d4b5ef5a4d5
Gavin Evans <oblivion.industries.gman@gmail.com>
2024-03-29 19:23:51 -0600

    Fixed Property Totals
    Property Totals was showing incorrect information, now it is fixed

--- WebApplication2/Pages/Listings/ViewProperty.cshtml.cs
+++ WebApplication2/Pages/Listings/ViewProperty.cshtml.cs
@@ -37,9 +37,7 @@ namespace FireBnBWeb.Pages.Listings
         public float TotalPriceForSevenNights { get; private set; }
 
 
-
-
-        public ViewPropertyModel(UnitofWork unitOfWork)
+		public ViewPropertyModel(UnitofWork unitOfWork)
         {
             _unitOfWork = unitOfWork;
         }
@@ -118,7 +116,7 @@ namespace FireBnBWeb.Pages.Listings
             totalPriceForSevenNights += PropertyFees.Sum(fee => fee.Fee.Percentage ?? 0.0);
 
             // Calculate total tax
-            double totalTax = totalPriceForSevenNights * (CityTax + CountyTax + StateTax);
+            double totalTax = totalPriceForSevenNights * ((CityTax + CountyTax + StateTax) / 100);
 
             // Subtract discounts from the total price
             totalPriceForSevenNights -= PropertyDiscounts.Sum(discount => discount.Discount.Value);
